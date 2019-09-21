5ac162c4c3fc7f3b0e912bd2ffa02d42ddba8f80
VyronLee <lwz_jz@hotmail.com>
2019-09-21 14:39:54 +0800

    Add LogLevel extensions.

--- vFrame.Core/Loggers/LogLevelDef.cs
+++ vFrame.Core/Loggers/LogLevelDef.cs
@@ -7,6 +7,9 @@
 //    Modified:  2018-10-20 18:06
 //   Copyright:  Copyright (c) 2018, VyronLee
 //============================================================
+
+using System;
+
 namespace vFrame.Core.Loggers
 {
     public enum LogLevelDef
@@ -16,5 +19,26 @@ namespace vFrame.Core.Loggers
         Warning = 4,
         Error = 8,
         Fatal = 16,
+    }
+
+    public static class LogLevelExtension
+    {
+        public static UnityEngine.LogType ToUnityLogLevel(this LogLevelDef level)
+        {
+            switch (Logger.LogLevel)
+            {
+                case LogLevelDef.Debug:
+                case LogLevelDef.Info:
+                    return UnityEngine.LogType.Log;
+                case LogLevelDef.Warning:
+                    return UnityEngine.LogType.Warning;
+                case LogLevelDef.Error:
+                    return UnityEngine.LogType.Assert;
+                case LogLevelDef.Fatal:
+                    return UnityEngine.LogType.Exception;
+                default:
+                    throw new ArgumentOutOfRangeException("level");
+            }
+        }
     }
 }
