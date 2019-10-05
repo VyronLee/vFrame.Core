8bcdf87eebc11ce4c289c31e91a1c3c5ef4b5478
VyronLee <lwz_jz@hotmail.com>
2019-10-05 10:20:38 +0800

    Add localization module.

---
+++ vFrame.Core/Localize/ILocalizationReader.cs
@@ -0,0 +1,21 @@
+﻿//------------------------------------------------------------
+//        File:  ILocalizationReader.cs
+//       Brief:  多语言数据读取接口
+//
+//      Author:  VyronLee, lwz_jz@hotmail.com
+//
+//    Modified:  2019-09-30 21:05
+//   Copyright:  Copyright (c) 2019, VyronLee
+//============================================================
+namespace vFrame.Core.Localize
+{
+    public interface ILocalizationReader
+    {
+        /// <summary>
+        /// 读取数据
+        /// </summary>
+        /// <param name="langCode">语言代码</param>
+        /// <returns>数据文本</returns>
+        string ReadData(string langCode);
+    }
+}
