7f8746ffa54cd48ba26123b7bca6e1c375bcd5ed
VyronLee <lwz_jz@hotmail.com>
2019-11-01 20:38:46 +0800

    Add input utils

---
+++ vFrame.Core/Utils/Unity/InputUtility.cs
@@ -0,0 +1,34 @@
+﻿//------------------------------------------------------------
+//        File:  InputUtility.cs
+//       Brief:  Input工具类
+//
+//      Author:  VyronLee, lwz_jz@hotmail.com
+//
+//    Modified:  2018-12-24 11:52
+//   Copyright:  Copyright (c) 2018, VyronLee
+//============================================================
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace vFrame.Core.Utils.Unity
+{
+    public static class InputUtils
+    {
+        /// <summary>
+        /// 是否点击在UI物件之上
+        /// </summary>
+        /// <param name="screenPosition">屏幕坐标</param>
+        /// <returns></returns>
+        public static bool IsPointOverUIObject(Vector2 screenPosition)
+        {
+            var eventData = new PointerEventData(EventSystem.current) {
+                position = new Vector2(screenPosition.x, screenPosition.y)
+            };
+            var results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventData, results);
+            return results.Count > 0;
+        }
+    }
+}
