5fa658bec30ee2e905550496f8395d419ebd3fda
vyronlee <lwz_jz@hotmail.com>
2020-02-01 22:11:23 +0800

    Fix compile scripts in macos.

--- BuildAllPlatform.sh
+++ BuildAllPlatform.sh
@@ -6,6 +6,8 @@ echo "Building for platform: Editor"
 msbuild vFrame.Core.sln /t:Clean,Rebuild /p:Configuration=Debug /p:Platform="Any CPU" /p:DefineConstants="TRACE DEBUG UNITY_EDITOR"
 mkdir -p ${CUR_DIR}/Output/vFrame.Core/Runtime
 cp -f ${CUR_DIR}/Build/vFrame.Core/Debug/vFrame.Core.* ${CUR_DIR}/Output/vFrame.Core/Runtime/
+mkdir -p ${CUR_DIR}/Output/vFrame.Core/Editor
+cp -f ${CUR_DIR}/Build/vFrame.Core.Editor/Debug/vFrame.Core.Editor.* ${CUR_DIR}/Output/vFrame.Core/Editor/
 
 echo "Building for platform: Standalone"
 msbuild vFrame.Core.sln /t:Clean,Rebuild /p:Configuration=Release /p:Platform="Any CPU" /p:DefineConstants="TRACE UNITY_STANDALONE"
