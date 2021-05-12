16e36f1ceaa895b3e32be7959a62ca2267473974
VyronLee <lwz_jz@hotmail.com>
2021-05-12 15:04:45 +0800

    Add SortedDictionaryPool.

---
+++ vFrame.Core.Lite/ObjectPools/Builtin/SortedDictionaryAllocator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace vFrame.Core.ObjectPools.Builtin
+{
+    public class SortedDictionaryAllocator<T1, T2> : IPoolObjectAllocator<SortedDictionary<T1, T2>>
+    {
+        public SortedDictionary<T1, T2> Alloc() {
+            return new SortedDictionary<T1, T2>();
+        }
+
+        public void Reset(SortedDictionary<T1, T2> obj) {
+            obj.Clear();
+        }
+    }
+}
