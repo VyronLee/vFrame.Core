3f7f2998bc5e790b9c67ee1227cdaa7ceb45dd8b
VyronLee <lwz_jz@hotmail.com>
2021-07-06 14:49:16 +0800

    Add box object.

---
+++ vFrame.Core.Lite/ObjectPools/Builtin/BoxAllocator.cs
@@ -0,0 +1,15 @@
+﻿using vFrame.Core.Base;
+
+namespace vFrame.Core.ObjectPools.Builtin
+{
+    public class BoxAllocator<T> : IPoolObjectAllocator<Box<T>>
+    {
+        public Box<T> Alloc() {
+            return new Box<T>();
+        }
+
+        public void Reset(Box<T> obj) {
+
+        }
+    }
+}
