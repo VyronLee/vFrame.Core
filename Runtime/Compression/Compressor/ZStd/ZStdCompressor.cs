01ec18ec15a6ffac70a64c7d69289a8f7a48b573
VyronLee <lwz_jz@hotmail.com>
2024-03-27 15:23:16 +0800

    Remove CreateAbility<> inheritance.

--- Runtime/Compression/Compressor/ZStd/ZStdCompressor.cs
+++ Runtime/Compression/Compressor/ZStd/ZStdCompressor.cs
@@ -21,9 +21,10 @@ namespace vFrame.Core.Compression
         }
 
         public override void Decompress(Stream input, Stream output, Action<long, long> onProgress) {
+            var options = Options as ZStdCompressorOptions ?? new ZStdCompressorOptions();
             using (var decoder = new ZstandardStream(input, CompressionMode.Decompress, true)) {
                 int length;
-                var buffer = new byte[Options.BuffSize];
+                var buffer = new byte[options.BuffSize];
                 while ((length = decoder.Read(buffer, 0, buffer.Length)) > 0) {
                     output.Write(buffer, 0, length);
                 }
