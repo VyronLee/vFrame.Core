67f802c9ffc3db8f0a8b760d41467e70aceb41ab
VyronLee <lwz_jz@hotmail.com>
2024-03-27 18:16:06 +0800

    AsyncRequest support self driven.

--- Assets/vFrame.Core.Unity/Runtime/Asynchronous/AsyncRequestCtrl.cs
+++ Assets/vFrame.Core.Unity/Runtime/Asynchronous/AsyncRequestCtrl.cs
@@ -10,14 +10,31 @@
 
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 using vFrame.Core.Base;
 using vFrame.Core.Exceptions;
+using vFrame.Core.Unity.Extensions;
 
 namespace vFrame.Core.Unity.Asynchronous
 {
     public class AsyncRequestCtrl : BaseObject, IAsyncRequestCtrl
     {
         private List<IAsyncRequest> _requests;
+        private UpdateDriver _driver;
+
+        private static AsyncRequestCtrl _shared;
+
+        public static AsyncRequestCtrl Shared {
+            get {
+                if (_shared != null) {
+                    return _shared;
+                }
+                _shared = new AsyncRequestCtrl();
+                _shared.Create();
+                _shared.SelfDrive();
+                return _shared;
+            }
+        }
 
         protected override void OnCreate() {
             _requests = new List<IAsyncRequest>(64);
@@ -26,10 +43,26 @@ namespace vFrame.Core.Unity.Asynchronous
         protected override void OnDestroy() {
             _requests?.Clear();
             _requests = null;
+
+            if (null != _driver) {
+                _driver.gameObject.DestroyEx();
+                _driver = null;
+            }
+        }
+
+        public void SelfDrive() {
+            if (null != _driver) {
+                return;
+            }
+            _driver = new GameObject(nameof(AsyncRequestCtrl))
+                .DontDestroyEx()
+                .DontSaveAndHideEx()
+                .AddComponent<UpdateDriver>();
+            _driver.Ctrl = this;
         }
 
         public void Update() {
-            ThrowIfDestroyed();
+            ThrowIfNotCreatedOrDestroyed();
 
             for (var i = _requests.Count - 1; i >= 0; i--) {
                 var request = _requests[i];
@@ -57,12 +90,12 @@ namespace vFrame.Core.Unity.Asynchronous
         }
 
         public T CreateRequest<T>() where T : IAsyncRequest {
-            ThrowIfDestroyed();
+            ThrowIfNotCreatedOrDestroyed();
             return (T) CreateRequest(typeof(T));
         }
 
         public IAsyncRequest CreateRequest(Type type) {
-            ThrowIfDestroyed();
+            ThrowIfNotCreatedOrDestroyed();
             if (!(Activator.CreateInstance(type) is AsyncRequest request)) {
                 throw new AsyncRequestTypeErrorException();
             }
@@ -72,18 +105,27 @@ namespace vFrame.Core.Unity.Asynchronous
         }
 
         public void AddRequest(IAsyncRequest request) {
-            ThrowIfDestroyed();
+            ThrowIfNotCreatedOrDestroyed();
             ThrowHelper.ThrowIfNull(request, nameof(request));
             _requests.Add(request);
         }
 
         public void RemoveRequest(IAsyncRequest request) {
-            ThrowIfDestroyed();
+            ThrowIfNotCreatedOrDestroyed();
             ThrowHelper.ThrowIfNull(request, nameof(request));
             _requests.Remove(request);
         }
 
         public event Action<IAsyncRequest> OnRequestFinish;
         public event Action<IAsyncRequest> OnRequestError;
+
+        private class UpdateDriver : MonoBehaviour
+        {
+            public AsyncRequestCtrl Ctrl { get; set; }
+
+            private void Update() {
+                Ctrl?.Update();
+            }
+        }
     }
 }
